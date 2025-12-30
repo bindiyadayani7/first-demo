786fd6ef78a3031b3cc4fa213fad02b4747c90c9
Bindiya <bindiyadayani7@gmail.com>
2025-12-30 10:46:36 +0000

    conversin of datatypes

---
+++ .github/01_demo/datatypes.js
@@ -0,0 +1,12 @@
+console.log(3+3);
+console.log(typeof "B");
+let LId= Number(12);
+console.log(typeof LId);
+
+// datatype conversion
+let score= 33
+let scoreconversion=String(score)
+console.log(scoreconversion);
+console.log(typeof scoreconversion);
+
+
